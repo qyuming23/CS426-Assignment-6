026eb4a80c3e105c5c8e65b2accd57f3391c0a0a
Ablang-E <ablange.eric@gmail.com>
2021-04-13 23:03:56 -0500

    Removed bullet script.

---
+++ Assets/push_bulletScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class push_bulletScript : MonoBehaviour
+{
+    public float power;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Vector3 direction = collision.transform.position - transform.position;
+            rb.AddForce(direction.normalized * power, ForceMode.Impulse);
+            MeshRenderer targetMesh = GetComponent<MeshRenderer>();
+        }
+
+    }
+}
