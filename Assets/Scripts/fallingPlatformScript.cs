c5b97912802f8421285da8744058c07529d81820
Ablang-E <ablange.eric@gmail.com>
2021-04-20 17:08:56 -0500

    4th level added.

--- Assets/Scripts/fallingPlatformScript.cs
+++ Assets/Scripts/fallingPlatformScript.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class fallingPlatformScript : MonoBehaviour
 {
     public Rigidbody rb;
+    Vector3 originalPos;
+    void Start()
+    {
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+
     IEnumerator TestCoroutine()
     {
         Debug.Log("about to yield return WaitForSeconds(1)");
         yield return new WaitForSeconds(2);
         Debug.Log("Just waited 1 second");
-        rb.isKinematic = false;
+        gameObject.transform.position = originalPos;
+        rb.isKinematic = true;
         Debug.Log("Just waited another second");
         yield break;
         Debug.Log("You'll never see this"); // produces a dead code warning
@@ -23,7 +30,7 @@ public class fallingPlatformScript : MonoBehaviour
         //StartCoroutine(TestCoroutine());
         Debug.Log("Back from StartCoroutine");
         //ield return new WaitForSeconds(3);
-        
+
     }
 
 
@@ -31,6 +38,6 @@ public class fallingPlatformScript : MonoBehaviour
     {
         rb.isKinematic = false;
         //StartCoroutine(TestCoroutine());
-        
+
     }
 }
