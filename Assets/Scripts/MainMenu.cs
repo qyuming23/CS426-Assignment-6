17ad285392a7899bcd1cd63bb3a6b66cf5caf6f6
qyuming23 <78322415+qyuming23@users.noreply.github.com>
2021-04-20 18:48:00 -0500

    Control and credit screen added

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,4 +8,8 @@ public class MainMenu : MonoBehaviour
     public void playGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void quitGame(){
+        Application.Quit();
+    }
 }
